70f882f83d3b6f0cc40b7a1f7585dd1f686bfce9
Ali Elkoca <>
2021-02-07 13:53:29 +0300

    initial commit

---
+++ CalculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by Ali Elkoca on 7.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
