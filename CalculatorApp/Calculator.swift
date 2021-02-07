9f9ea26e872a3ab6fd8f5495a2996dcdf0e932da
Ali Elkoca <>
2021-02-07 16:52:10 +0300

    last commit

---
+++ CalculatorApp/Calculator.swift
@@ -0,0 +1,20 @@
+//
+//  Calculator.swift
+//  CalculatorApp
+//
+//  Created by Ali Elkoca on 7.02.2021.
+//
+
+import Foundation
+
+class Calculator {
+    
+    func add(a:Int,b:Int) -> Int {
+        return a+b
+    }
+    
+    func subtract(a:Int,b:Int) -> Int {
+        return a-b
+    }
+        
+}
